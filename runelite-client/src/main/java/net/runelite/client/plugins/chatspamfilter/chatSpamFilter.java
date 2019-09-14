bc7594b1ec74dfd0a998a5b53e7bf53c012bb2e0
AmineGherib <AmineGherib@github.com>
2019-09-14 00:12:41 -0400

    Added chatSpamFilter package and class.

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatspamfilter/chatSpamFilter.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.chatspamfilter;
+
+import net.runelite.client.plugins.PluginDescriptor;
+
+@PluginDescriptor(
+        name = "Chat Spam Filter",
+        description = "Detects spam autochat messages and filters them out",
+        tags = {"duel", "messages", "notifications", "trade", "username"},
+        enabledByDefault = false
+)
+public class chatSpamFilter {
+}
